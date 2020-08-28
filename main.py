55b3ddd56048b20cd2924a61dbcc65f339b99520
jjm07 <jjm7410@psu.edu>
2020-08-28 20:48:35 +0000

    alkdfj

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-print("input temperature in celsius")
-celsius = ("input temperature in celsius")
-# calculate fahrenheit
-fahrenheit = (celsius * 1.8) + 32
-Print(/str(celsius)"in celsius is equivalent to (str(fahrenheit")
+temp = input("Enter temperature in celsius: ")
+temp = int(temp)
+ntemp = temp*(9/5)+32
+print(f"{temp}° in Celsius is equivalent to {ntemp}° in Farenheit.")
