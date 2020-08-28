ec5fcaea5b91196133d3c3788b2a7b0e791174a8
jjm07 <jjm7410@psu.edu>
2020-08-28 20:23:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("input temperature in celsius")
+celsius = ("input temperature in celsius")
+# calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+Print(/str(celsius)"in celsius is equivalent to (str(fahrenheit")
